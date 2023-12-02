c49fe98bef44f53ca953c4503a9cdbe27e248ce9
Hanyi Zhou <133040296+hanyi-zhou@users.noreply.github.com>
2023-12-02 13:30:23 -0800

    Update app.py

--- api/app.py
+++ api/app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, origins='*')
+CORS(app, origins="*")
 
 from program_runner import run_program_with_input
 
