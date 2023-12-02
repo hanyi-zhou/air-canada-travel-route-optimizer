fe1ee9f4672aa711abef2c4cb9fdf9e5601ef3fe
Hanyi Zhou <133040296+hanyi-zhou@users.noreply.github.com>
2023-12-02 13:31:32 -0800

    Update program_runner.py

--- api/program_runner.py
+++ api/program_runner.py
@@ -2,7 +2,7 @@ import subprocess
 
 def run_program_with_input(input_str):
     # Run the Python program using subprocess
-    process = subprocess.Popen(["python", "process.py"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, text=True)
+    process = subprocess.Popen(["python", "process_dp.py"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, text=True)
 
     # Write the input to the program's standard input
     process.stdin.write(input_str)
